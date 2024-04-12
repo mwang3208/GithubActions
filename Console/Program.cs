e7b1d6a9866049a5837546f9f2f59793d246ec26
mwang3208 <mwang32@huskers.unl.edu>
2024-04-12 14:16:20 -0500

    Revert "added power function"
    This reverts commit bedb31987fb83bc8c5ba381058f9174f2eeae6c6.

--- Console/Program.cs
+++ Console/Program.cs
@@ -80,9 +80,14 @@ namespace GithubActionsLab
         {
             return double.Parse(x) / double.Parse(y);
         }
+
+        // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
-            return Math.Pow(double.Parse(x),double.Parse(y));
+            throw new NotImplementedException();
         }
     }
+
+
+
 }
